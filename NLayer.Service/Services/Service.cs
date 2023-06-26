4d12269199d29af3405efc965832f92d99a247cd
Yaşar İsmet Yılmaz <yasar3814@gmail.com>
2023-06-26 10:49:17 +0300

    Update.

---
+++ NLayer.Service/Services/Service.cs
@@ -0,0 +1,68 @@
+﻿using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Service.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+        private readonly IGenericRepository<T> _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public Service(IUnitOfWork unitOfWork, IGenericRepository<T> repository)
+        {
+            _unitOfWork = unitOfWork;
+            _repository = repository;
+        }
+
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AnyAsync(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveRangeAsync(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> Where(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
